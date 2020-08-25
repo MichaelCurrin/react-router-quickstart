aa8cee888c69ecefabd43c5f31b941304d95fadd
Michael Currin <michael.currin@gmail.com>
2020-08-26 00:04:32 +0200

    refactor: Update Topics.js

--- src/components/Topics.js
+++ src/components/Topics.js
@@ -2,13 +2,22 @@ import React from 'react';
 
 import { Switch, Route, Link, useRouteMatch, useParams } from 'react-router-dom';
 
+function Topic() {
+  let { topicId } = useParams();
+  return <h3>Requested topic ID: {topicId}</h3>;
+}
+
 export function Topics() {
   let match = useRouteMatch();
 
   return (
     <div>
       <h2>Topics</h2>
-      <p>This demonstrates how nested routing works.</p>
+      <p>
+        This demonstrates how dynamic nested routing works - the result is based on the input value
+        and not a prepared element. And use of match.url and match.path makes it easy to only worry
+        about the last part of the path that matters for this component.
+      </p>
 
       <ul>
         <li>
@@ -29,9 +38,4 @@ export function Topics() {
       </Switch>
     </div>
   );
-}
-
-function Topic() {
-  let { topicId } = useParams();
-  return <h3>Requested topic ID: {topicId}</h3>;
 }
